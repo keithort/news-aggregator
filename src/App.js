85c9f84bf9c744ef09ffa4d2b1feba8f19cab489
Keith Ort <keith@verticallatency.com>
2018-11-09 14:00:37 -0500

    Remove Pose for now

--- src/App.js
+++ src/App.js
@@ -1,6 +1,5 @@
 import React from "react";
 import { Route, Switch } from "react-router-dom";
-import posed, { PoseGroup } from "react-pose";
 import { Provider } from "react-redux";
 import store from "./store";
 
@@ -11,11 +10,6 @@ import Footer from "./Components/Footer";
 import Home from "./Components/Home";
 import Feed from "./Components/Feed";
 
-const RouteContainer = posed.div({
-  enter: { opacity: 1, delay: 100, beforeChildren: true },
-  exit: { opacity: 0 }
-});
-
 const App = () => (
   <Provider store={store}>
     <Route
@@ -23,31 +17,27 @@ const App = () => (
         <>
           <Header />
           <Layout>
-            <PoseGroup>
-              <RouteContainer key={location}>
-                <Switch location={location}>
-                  <Route exact path="/" component={Home} key="home" />
-                  <Route
-                    exact
-                    path="/science"
-                    render={props => <Feed type="Science" />}
-                    key="science"
-                  />
-                  <Route
-                    exact
-                    path="/sports"
-                    render={props => <Feed type="Sports" />}
-                    key="sports"
-                  />
-                  <Route
-                    exact
-                    path="/technology"
-                    render={props => <Feed type="Technology" />}
-                    key="technology"
-                  />
-                </Switch>
-              </RouteContainer>
-            </PoseGroup>
+            <Switch location={location}>
+              <Route exact path="/" component={Home} key="home" />
+              <Route
+                exact
+                path="/science"
+                render={props => <Feed type="Science" />}
+                key="science"
+              />
+              <Route
+                exact
+                path="/sports"
+                render={props => <Feed type="Sports" />}
+                key="sports"
+              />
+              <Route
+                exact
+                path="/technology"
+                render={props => <Feed type="Technology" />}
+                key="technology"
+              />
+            </Switch>
           </Layout>
           <Footer />
         </>
